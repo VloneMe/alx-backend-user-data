2206adb2812f88bcc80b352a20f624795486660f
Vlone Me <francisntima@gmail.com>
2024-02-09 01:41:53 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,42 +1,141 @@
 #!/usr/bin/env python3
-"""
-Provides functions for encrypting and validating passwords using bcrypt.
-"""
+"""Demonstrates the use of regex to replace
+occurrences of specific field values."""
+import re
+import logging
+import mysql.connector
+import os
+from typing import List, Tuple
 
-import bcrypt
-from typing import Union
+
+class RedactingFormatter(logging.Formatter):
+    """A custom formatter to redact sensitive information from log messages."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initializes the RedactingFormatter.
+
+        Args:
+            fields: A list of strings representing sensitive fields to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record with redacted sensitive information.
+
+        Args:
+            record: A LogRecord object containing log information.
+
+        Returns:
+            A string representing the formatted log message.
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
-def hash_password(password: str) -> bytes:
+PII_FIELDS = ("name", "email", "password", "ssn", "phone")
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Establishes connection to the MySQL environment."""
+    try:
+        db_connect = mysql.connector.connect(
+            user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+            password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+            host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+            database=os.getenv('PERSONAL_DATA_DB_NAME')
+        )
+        return db_connect
+    except mysql.connector.Error as e:
+        logging.error("Error connecting to database: %s", e)
+        raise
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
-    Hashes the provided password using bcrypt.
+    Redacts sensitive information from log messages using regex.
 
     Args:
-        password: A string representing the password to be hashed.
+        fields: A list of strings representing sensitive fields to redact.
+        redaction: A string representing the redacted value.
+        message: A string representing the log message to be filtered.
+        separator: A string representing
+        the separator between field-value pairs.
 
     Returns:
-        A byte string representing the hashed password.
+        A string representing the filtered log message.
     """
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-    return hashed
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
 
 
-def is_valid(hashed_password: Union[str, bytes], password: str) -> bool:
+def get_logger() -> logging.Logger:
     """
-    Validates the provided password against the hashed password.
+    Returns a configured logging.Logger object.
+
+    Returns:
+        A logging.Logger object configured with a RedactingFormatter.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    target_handler = logging.StreamHandler()
+    target_handler.setLevel(logging.INFO)
+
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    target_handler.setFormatter(formatter)
+
+    logger.addHandler(target_handler)
+    return logger
+
+
+def fetch_users(cursor: mysql.connector.cursor.MySQLCursor) -> List[Tuple]:
+    """
+    Retrieves all users from the database.
 
     Args:
-        hashed_password: A string or
-        bytes object representing the hashed password.
-        password: A string representing the password to be validated.
+        cursor: A MySQLCursor object used to execute SQL queries.
 
     Returns:
-        True if the provided password matches
-        the hashed password, False otherwise.
+        A list of tuples representing user records.
     """
-    if isinstance(hashed_password, str):
-        hashed_password = hashed_password.encode()
+    cursor.execute("SELECT * FROM users;")
+    return cursor.fetchall()
 
-    encoded = password.encode()
-    return bcrypt.checkpw(encoded, hashed_password)
+
+def main() -> None:
+    """
+    Retrieves user data from the database and
+    logs it with sensitive information redacted.
+    """
+    db = get_db()
+    cursor = db.cursor()
+
+    try:
+        users = fetch_users(cursor)
+        headers = [field[0] for field in cursor.description]
+        logger = get_logger()
+
+        for row in users:
+            info_answer = ''
+            for f, p in zip(row, headers):
+                info_answer += f'{p}={(f)}; '
+            logger.info(info_answer)
+
+    finally:
+        cursor.close()
+        db.close()
+
+
+if __name__ == '__main__':
+    main()
