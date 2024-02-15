5adcc6bb57bba209057d22b870cf16faa78f628f
Vlone Me <francisntima@yahoo.com>
2024-02-15 18:32:43 +0000

    Et moi et moi et moi!

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+This script initializes a Flask application for an API
+and handles authentication and authorization for its routes.
+"""
+
+import os
+from os import getenv
+from typing import Tuple
+
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+
+auth_type = getenv('AUTH_TYPE', 'default')
+if auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif auth_type == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif auth_type == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+elif auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> Tuple[jsonify, int]:
+    """ Not found handler """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error: Exception) -> Tuple[jsonify, int]:
+    """Error handler for unauthorized requests."""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error: Exception) -> Tuple[jsonify, int]:
+    """Error handler for unauthorized requests."""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def handle_request():
+    """
+    Handle the request by checking for
+    authentication and authorization.
+    """
+    if auth is None:
+        return
+    
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+    
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
+        abort(401)
+    
+    user = auth.current_user(request)
+    if user is None:
+        abort(403)
+    
+    request.current_user = user
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port, debug=True)
