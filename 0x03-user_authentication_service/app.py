1e08948cc261ed2f1606d1b276013357cccb6787
Vlone Me <francisntima@yahoo.com>
2024-02-23 02:15:41 +0000

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""A basic Flask app for user authentication."""
+""" a basic Flask app. """
 from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
+
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -9,25 +10,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def welcome():
-    """Returns a welcome message."""
-    return jsonify({"message": "Welcome"})
+    """ returns a message when the route / is requested """
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'])
 def users():
-    """Registers a new user."""
+    """ registers a new user """
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "User created"})
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
-        return jsonify({"message": "Email already registered"})
+        return jsonify({"message": "email already registered"})
 
 
 @app.route('/sessions', methods=['POST'])
 def sessions():
-    """Creates a new session for the user and returns a JSON payload."""
+    """ create a new session for the user,
+    store it the session ID as a cookie with key "session_id" on the response
+    and return a JSON payload """
     email = request.form.get('email')
     password = request.form.get('password')
     if not AUTH.valid_login(email, password):
@@ -35,14 +38,14 @@ def sessions():
     session_id = AUTH.create_session(email)
     if not session_id:
         abort(401)
-    response = make_response(jsonify({"email": email, "message": "Logged in"}))
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
     response.set_cookie("session_id", session_id)
     return response
 
 
 @app.route('/sessions', methods=['DELETE'])
 def logout():
-    """Logs out the user by destroying the session."""
+    """ user logout by destroying the session_id """
     cookie = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(cookie)
     if user is None:
@@ -54,7 +57,7 @@ def logout():
 
 @app.route('/profile', methods=['GET'])
 def profile():
-    """Returns the profile of the user."""
+    """ finds the corresponding user """
     cookie = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(cookie)
     if user is None:
@@ -65,7 +68,7 @@ def profile():
 
 @app.route('/reset_password', methods=['POST'])
 def get_reset_password_token():
-    """Responds with a reset token."""
+    """ responds with a reset_token """
     email = request.form.get('email')
     try:
         reset_token = AUTH.get_reset_password_token(email)
@@ -78,7 +81,7 @@ def get_reset_password_token():
 
 @app.route('/reset_password', methods=['PUT'])
 def update_password():
-    """Updates the user password."""
+    """ updates the user password """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
